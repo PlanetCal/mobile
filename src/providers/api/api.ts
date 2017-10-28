8b4dd7d7627b58f565f5ab1fbb878b716d38616a
khatauli <sachin.kumar.v@gmail.com>
2017-10-28 06:26:54 -0700

    Changed the project to blank template

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,47 +1,18 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
-/**
- * Api is a generic REST Api handler. Set your API url first.
- */
+/*
+  Generated class for the ApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
 @Injectable()
-export class Api {
-  url: string = 'http://localhost:1337';
+export class ApiProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(public http: Http) {
+    console.log('Hello ApiProvider Provider');
   }
 
-  get(endpoint: string, params?: any, reqOpts?: any) {
-    if (!reqOpts) {
-      reqOpts = {
-        params: new HttpParams()
-      };
-    }
-
-    // Support easy query params for GET requests
-    if (params) {
-      reqOpts.params = new HttpParams();
-      for (let k in params) {
-        reqOpts.params.set(k, params[k]);
-      }
-    }
-
-    return this.http.get(this.url + '/' + endpoint, reqOpts);
-  }
-
-  post(endpoint: string, body: any, reqOpts?: any) {
-    return this.http.post(this.url + '/' + endpoint, body, reqOpts);
-  }
-
-  put(endpoint: string, body: any, reqOpts?: any) {
-    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
-  }
-
-  delete(endpoint: string, reqOpts?: any) {
-    return this.http.delete(this.url + '/' + endpoint, reqOpts);
-  }
-
-  patch(endpoint: string, body: any, reqOpts?: any) {
-    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
-  }
 }

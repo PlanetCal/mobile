576a61e8e295a0c960820b8d9c75f632e447040f
khatauli <sachin.kumar.v@gmail.com>
2017-11-11 23:09:26 -0800

    Added reset password option

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,8 @@ export class MyApp {
 
     this.loggedInPages = [
       { title: 'Settings', component: 'SettingsPage', icon: 'settings' },
-      { title: 'Logout', component: 'LogoutPage', icon: 'log-out' }
+      { title: 'Logout', component: 'LogoutPage', icon: 'log-out' },
+      { title: 'Reset Password', component: 'LoginPage', icon: 'refresh' }
     ];
 
     this.loggedOutPages = [

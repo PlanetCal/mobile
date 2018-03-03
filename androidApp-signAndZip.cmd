eb8bb83367f31f84a429936fbba6c23961dab7fa
khatauli <sachin.kumar.v@gmail.com>
2018-03-03 11:50:08 -0800

    Updated the cmd file for publishing an app.

--- androidApp-signAndZip.cmd
+++ androidApp-signAndZip.cmd
@@ -1,3 +1,5 @@
-jarsigner -verbose -keystore keyStoreFile\planetcal.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk planetcalkey
+jarsigner -verbose -keystore keyStoreFile\planetcal2048.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk PlanetCal
 jarsigner -verify platforms\android\build\outputs\apk\android-release-unsigned.apk
-zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk platforms\android\build\outputs\apk\PlanetCal.apk
+zipalign -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk platforms\android\build\outputs\apk\android-release-unsigned-aligned.apk
+apksigner sign --ks keyStoreFile\planetcal2048.keystore --ks-pass pass:Testing123 --key-pass pass:Testing123 --out platforms\android\build\outputs\apk\android-release-signed.apk platforms\android\build\outputs\apk\android-release-unsigned-aligned.apk
+
